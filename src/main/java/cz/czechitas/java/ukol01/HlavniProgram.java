e7fe7413eb05cfeb34b81ee8580bff6d4023539f
Petra Kryk <petra.krykorkova@gmail.com>
2024-01-28 18:43:13 +0100

    Část 3 - Obrázek

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,23 +11,46 @@ public class HlavniProgram {
 
   public void start() {
     zofka = new Turtle();
-    nakresliPrasatko();
+    nakresliPrasatko(); /*začátek prasátko*/
+    zofka.turnRight(90);
+    zofka.move(180);
+    zofka.turnLeft(90);
+    zofka.move(60);
+    nakresliDomecek(); /*začátek domeček vpravo*/
+    zofka.turnRight(90);
+    zofka.move(150);
+    zofka.turnRight(90);
+    zofka.move(50);
+    zofka.turnLeft(90);
+    for (int i = 0; i < 5; i++) { /*cyklus domečky nahoře*/
+      nakresliDomecek();
+      zofka.move(50);
+      zofka.turnRight(90);
+    }
+    zofka.turnRight(30); /*začátek sluníčko*/
+    zofka.move(130);
+    nakresliSlunicko();
+    zofka.turnRight(147);
+    zofka.move(250);
+    zofka.turnRight(180);
+    nakresliDomecek();
   }
 
   public void nakresliDomecek() {
     zofka.penDown();
     zofka.turnRight(180);
     for (int i = 0; i < 2; i++) {
-      zofka.move(150);
+      zofka.move(60);
       zofka.turnRight(90);
-      zofka.move(100);
+      zofka.move(50);
       zofka.turnRight(90);
     }
     zofka.turnLeft(215);
-    zofka.move(100);
+    zofka.move(50);
     zofka.turnLeft(117);
-    zofka.move(100);
+    zofka.move(50);
     zofka.turnRight(62);
+    zofka.penUp();
   }
 
   public void nakresliPrasatko() {
@@ -36,27 +59,27 @@ public class HlavniProgram {
     zofka.turnRight(45);
     nakresliNozicky();
     zofka.turnRight(135);
-    zofka.move(145);
+    zofka.move(50);
     zofka.turnRight(135);
     nakresliNozicky();
     zofka.turnRight(45);
+    zofka.penUp();
   }
 
   public void nakresliNozicky() {
     zofka.penDown();
-    zofka.move(30);
+    zofka.move(15);
     zofka.turnRight(180);
     zofka.penUp();
-    zofka.move(30);
+    zofka.move(15);
     zofka.turnRight(90);
     zofka.penDown();
-    zofka.move(30);
+    zofka.move(15);
     zofka.penUp();
     zofka.turnRight(180);
-    zofka.move(30);
+    zofka.move(15);
     zofka.penUp();
   }
-/*
   public void nakresliOsmiuhelnik() {
     for (int i = 0; i < 8; i++) {
       zofka.turnRight(45);
@@ -64,31 +87,29 @@ public class HlavniProgram {
     }
     zofka.penUp();
   }
-
   public void nakresliKolecko() {
     zofka.penDown();
-    for (int i = 0; i < 72; i++) {
-      zofka.turnRight(5);
-      zofka.move(5);
+    for (int i = 0; i < 144; i++) {
+      zofka.turnRight(2.5);
+      zofka.move(1);
     }
     zofka.penUp();
   }
-
   public void nakresliSlunicko() {
     nakresliKolecko();
     zofka.penUp();
     zofka.turnRight(90);
-    zofka.move(57.297);
+    zofka.move(22.91899);
     zofka.turnLeft(90);
     for (int i = 0; i < 12; i++) {
-    zofka.move(57.297);
+    zofka.move(22.91899);
     zofka.penDown();
-    zofka.move(20);
+    zofka.move(10);
     zofka.turnLeft(180);
     zofka.penUp();
-    zofka.move(77.297);
+    zofka.move(32.91899);
     zofka.turnRight(30);
+    zofka.penUp();
     }
   }
-  */
 }
